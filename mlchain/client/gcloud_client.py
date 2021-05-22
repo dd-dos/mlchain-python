0a2d0e5b874181c6be24796e94638bff2dd4755b
dd-dos <dungphamdang99@gmail.com>
2021-05-23 06:23:16 +0700

    Support gcloud mlchain client + oauth2 authorization

---
+++ mlchain/client/gcloud_client.py
@@ -0,0 +1,31 @@
+import google.auth.transport.requests
+import google.oauth2.id_token
+import requests
+from mlchain import logger
+from mlchain.client import Client
+from mlchain.decorators import except_serving
+
+from . import Client
+
+
+class GcloudClient(Client)
+    def __init__(self, api_key=None, api_address=None, serializer='json', timeout=5 * 60, headers={}, type='http',
+                 name: str = "", version: str = "", check_status=False):
+        self.id_token = self.get_oauth2_token(api_address)
+        self.headers = {"Authorization": f"Bearer {token}"}
+        Client.__init__(self, api_key=api_key, api_address=api_address, serializer=serializer, timeout=timeout, headers=self.header, type=type,
+                        name=name, version=version, check_status=check_status)
+
+    def get_oauth2_token(self, url):
+        """
+        new_request creates a new HTTP request with IAM ID Token credential.
+        This token is automatically handled by private Cloud Run (fully managed)
+        and Cloud Functions.
+        """
+
+        auth_req = google.auth.transport.requests.Request()
+        target_audience = url
+
+        id_token = google.oauth2.id_token.fetch_id_token(auth_req, target_audience)
+
+        return id_token
