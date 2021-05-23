786cd5f05899ef77bfbb957f2a94907251ea82a6
dd-dos <dungphamdang99@gmail.com>
2021-05-23 07:08:04 +0700

    Move gcloud client to init

--- mlchain/client/__init__.py
+++ mlchain/client/__init__.py
@@ -1,5 +1,9 @@
+import google.auth.transport.requests
+import google.oauth2.id_token
+import requests
 from mlchain import mlconfig
 from mlchain.base import logger
+
 from .grpc_client import GrpcClient
 from .http_client import HttpClient
 
@@ -36,6 +40,29 @@ class Client(HttpClient, GrpcClient):
             return GrpcClient(api_key=self._api_key, api_address=self._api_address, serializer=self._serializer,
                               timeout=self._timeout, headers=self._headers, name=name, version=version,
                               check_status=check_status)
+
+
+class GcloudClient(Client):
+    def __init__(self, api_key=None, api_address=None, serializer='json', timeout=5 * 60, headers={}, type='http',
+                 name: str = "", version: str = "", check_status=False):
+        self.id_token = self._get_oauth2_token(api_address)
+        self.headers = {"Authorization": f"Bearer {self.id_token}"}
+        Client.__init__(self, api_key=api_key, api_address=api_address, serializer=serializer, timeout=timeout, headers=self.header, type=type,
+                        name=name, version=version, check_status=check_status)
+
+    def _get_oauth2_token(self, url):
+        """
+        new_request creates a new HTTP request with IAM ID Token credential.
+        This token is automatically handled by private Cloud Run (fully managed)
+        and Cloud Functions.
+        """
+
+        auth_req = google.auth.transport.requests.Request()
+        target_audience = url
+
+        id_token = google.oauth2.id_token.fetch_id_token(auth_req, target_audience)
+
+        return id_token
 
 
 def get_model(name):
